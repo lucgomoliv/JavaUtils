872d23e557c463f73d63f572952dc6b2b15b03ae
lucgomoliv <51489900+lucgomoliv@users.noreply.github.com>
2020-02-29 10:52:03 -0300

    Add files via upload

--- utils/Leitor.java
+++ utils/Leitor.java
@@ -11,34 +11,42 @@ public class Leitor {
         br = new BufferedReader(new InputStreamReader(System.in));
     }
 
+    /// Lê um número inteiro
     public int lerInt() throws NumberFormatException, IOException {
         return Integer.parseInt(br.readLine());
     }
 
+    /// Lê um número double
     public double lerDouble() throws NumberFormatException, IOException {
         return Double.parseDouble(br.readLine());
     }
 
+    /// Lê uma string
     public String lerString() throws NumberFormatException, IOException {
         return br.readLine();
     }
 
+    /// Lê um caracter
     public char lerChar() throws IOException {
         return br.readLine().charAt(0);
     }
 
+    /// Lê um número short
     public short lerShort() throws NumberFormatException, IOException {
         return Short.parseShort(br.readLine());
     }
 
+    /// Lê um número long
     public long lerLong() throws NumberFormatException, IOException {
         return Long.parseLong(br.readLine());
     }
 
+    /// Lê um número float
     public float lerFloat() throws NumberFormatException, IOException {
         return Float.parseFloat(br.readLine());
     }
 
+    /// Lê um booleano
     public boolean lerBoolean() throws IOException {
         return Boolean.parseBoolean(br.readLine());
     }
